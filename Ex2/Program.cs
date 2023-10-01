ba298bf7832a068fe50513adb88649a35c5a4d02
Kleo <mmttdd16@gmail.com>
2023-10-01 17:39:42 +0300

    Finished Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -5,5 +5,40 @@
 // 0 1 2 3
 // 1 2 3 4
 // 2 3 4 5
+int rows = WorkWithUser("Введите кол-во строк: ");
+int columns = WorkWithUser("Введите кол-во столбцов: ");
+int[,] matix = GetArray(rows, columns);
+PrintArray(matix);
 
+int WorkWithUser(string message)
+{
+        System.Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[,] GetArray(int row, int column)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) для строчик
+    { 
+        for (int j = 0; j < inArray.GetLength(1); j++) //GetLength(1) для столбца
+        {
+            System.Console.Write($"{inArray[i, j]} "); 
+        }
+        System.Console.WriteLine();
+    }
+}
 
