af031a663d60b44e71ef90df8dc7e6905cefbe0a
Kleo <mmttdd16@gmail.com>
2023-10-01 16:55:35 +0300

    Finished Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int rows = WorkWithUser("Введите кол-во строк: ");
+int columns = WorkWithUser("Введите кол-во столбцов: ");
+int minValue = WorkWithUser("Введите минимальное значение: ");
+int maxValue = WorkWithUser("Введите максимальное значение: ");
+int[,] array = GetArray(rows, columns, minValue, maxValue + 1);
+PrintArray(array);
+
+int WorkWithUser(string message)
+{
+        System.Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[,] GetArray(int row, int column, int minValue, int maxValue)
+{
+    int[,] result = new int[row, column];
+    Random rnd = new Random();
+    for(int i = 0; i < row; i++)
+    { 
+        for (int j = 0; j < column; j++)
+        {
+            result[i, j] = rnd.Next(minValue, maxValue); 
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) для строчик
+    { 
+        for (int j = 0; j < inArray.GetLength(1); j++) //GetLength(1) для столбца
+        {
+            System.Console.Write($"{inArray[i, j]} "); 
+        }
+        System.Console.WriteLine();
+    }
+}
+
